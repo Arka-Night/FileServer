87c3295af1fb6605059a9b2fb4840219396ca804
rEiNbU-gAi <elonneto@outlook.com>
2020-11-25 17:24:57 -0300

    update

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const cors = require('cors');
 const app = express();
 const routes = require('./src/routes');
 const port = 45033;
+const corsConfig = {
+	origin: ['http://168.228.219.200:45033', 'http://172.16.3.30:45033']
+};
 
 app.use(express.json({ limit: '500mb' }));
-app.use(cors());
+app.use(cors(corsConfig));
 app.use(routes);
 
 app.listen(port, () => {
